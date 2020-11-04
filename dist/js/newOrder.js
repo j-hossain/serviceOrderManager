862190988e26007e603f677208f8d81938ff1048
j-hossain <jahin.hsn101@gmail.com>
2020-11-04 23:42:24 +0600

    form structures created for new order

--- dist/js/newOrder.js
+++ dist/js/newOrder.js
@@ -1,4 +1,15 @@
 function newOrder(){
     var body = cleanBody();
-    
+    document.getElementsByTagName
+}
+
+function counterDecrease(ele){
+    var numberBox = ele.parentElement.getElementsByTagName('input')[0];
+    if(numberBox.value>0)
+        numberBox.value--;
+}
+
+function counterIncrease(ele){
+    var numberBox = ele.parentElement.getElementsByTagName('input')[0];
+    numberBox.value++;
 }
